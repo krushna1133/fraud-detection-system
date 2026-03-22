29cecc06947e33cdbb4c9169434678b53866a597
Krushna Rathod <krushnarathod0077@gmail.com>
2026-03-22 11:58:36 +0530

    ml-model port 8080

--- ml-model/ml_api.py
+++ ml-model/ml_api.py
@@ -3,6 +3,8 @@ from flask import Flask, request, jsonify
 import joblib
 
 app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 # Load model once (important)
 model = joblib.load("fraud_model.pkl")
@@ -29,7 +31,3 @@ def predict():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
